5bf165b455d5b66a6ecd860dbc46d19662a66ac7
gepirvu <pirvugeorgian.bi@gmail.com>
2025-05-23 19:41:58 +0200

    chore: fix import sorting with Ruff

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,20 +1,18 @@
 """Conftest module."""
 
+import platform
+
 from insurance import PROJECT_DIR
 
-# Define directories
 MLRUNS_DIR = PROJECT_DIR / "tests" / "mlruns"
 CATALOG_DIR = PROJECT_DIR / "tests" / "catalog"
+CATALOG_DIR.mkdir(parents=True, exist_ok=True)  # noqa
 
-# Ensure directories exist
-CATALOG_DIR.mkdir(parents=True, exist_ok=True)
-MLRUNS_DIR.mkdir(parents=True, exist_ok=True)
+# To make the TRACKING_URI  path compatible for both macOS and Windows
+if platform.system() == "Windows":
+    TRACKING_URI = f"file:///{MLRUNS_DIR.as_posix()}"
+else:
+    TRACKING_URI = f"file://{MLRUNS_DIR.as_posix()}"
 
-# Use URI-safe absolute path
-TRACKING_URI = MLRUNS_DIR.resolve().as_uri()
 
-# Register fixtures
-pytest_plugins = [
-    "tests.fixtures.datapreprocessing_fixture",
-    "tests.fixtures.custom_model_fixture",
-]
+pytest_plugins = ["tests.fixtures.datapreprocessing_fixture", "tests.fixtures.custom_model_fixture"]
